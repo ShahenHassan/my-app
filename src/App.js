d9fae6d50181fdef3ea7bb65a21d24f91c56bf98
shahen2024 <shahen.hasan.h@gmail.com>
2023-03-05 13:11:45 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+// import {Cards} from './component/showCard/Cards';
+import  {Article}  from './component/showCard/Article';
+function App() {
+  return (
+  <div>
+      <>
+      {/* <Cards /> */}
+      <Article />
+      
+   
+    </>
+  </div>
+    
+  );
+}
+
+export default App;
